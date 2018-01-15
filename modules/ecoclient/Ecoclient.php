548fb50e56753dfb8c088e1024c2f9e5af85532f
shap <sanatarchuk@gmail.com>
2018-01-15 12:40:52 +0200

    const MAX_AVERAGE_DAILY_DUST_LEVEL = 25;

--- modules/ecoclient/Ecoclient.php
+++ modules/ecoclient/Ecoclient.php
@@ -14,6 +14,8 @@ class Ecoclient extends Module
 
     const defaultDbTablesPrefix = '';
     const MAX_AVERAGE_YEAR_DUST_LEVEL = 10;
+    const MAX_AVERAGE_DAILY_DUST25_LEVEL = 25;
+    const MAX_AVERAGE_DAILY_DUST10_LEVEL = 50;
 
     public function init()
     {
